3829d67b8f65995629ff0b4a155c9d8804896b15
Jesús Barros <jesus.reinaldo.barros@gmail.com>
2023-02-15 20:32:51 -0500

    Add :: theme

---
+++ styles/theme.js
@@ -0,0 +1,14 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const colors = {
+  styles: {
+    global: (props) => ({
+      body: {
+        margin: 0,
+        margin: 0,
+      },
+    }),
+  },
+}
+
+const theme = extendTheme({ colors })
